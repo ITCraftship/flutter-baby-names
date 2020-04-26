b06fb8ca5801b93da8d90911b5d5575378597dd5
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2020-04-26 19:59:21 +0200

    Using media picker to select image from the gallery

--- lib/pages/media_page.dart
+++ lib/pages/media_page.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MediaPage extends StatelessWidget {
+  Future getImage(BuildContext context) async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text(
+        'Image added ${image.uri}!',
+        style: TextStyle(color: Theme.of(context).textTheme.bodyText1.color),
+      ),
+      backgroundColor: Theme.of(context).dialogBackgroundColor,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,15 +20,8 @@ class MediaPage extends StatelessWidget {
         child: Text('Media'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Scaffold.of(context).showSnackBar(SnackBar(
-            content: Text(
-              'Image added!',
-              style:
-                  TextStyle(color: Theme.of(context).textTheme.bodyText1.color),
-            ),
-            backgroundColor: Theme.of(context).dialogBackgroundColor,
-          ));
+        onPressed: () async {
+          await getImage(context);
         },
         child: Icon(Icons.add),
       ),
