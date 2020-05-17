3669901ef39df9a0810ac8fdc682034dc503b746
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2020-05-17 18:37:23 +0200

    Added fake_async dependency Added bloc tests using fake_async

--- test/blocs/name_votes/name_votes_bloc_test.dart
+++ test/blocs/name_votes/name_votes_bloc_test.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:fake_async/fake_async.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -58,17 +59,42 @@ void main() {
         wait: const Duration(milliseconds: 0), // need to add wait, so that the listen on all() adds an event to the block
         expect: [NameVotesLoaded(nameVotes: nameVotes)]);
 
-    // blocTest(
-    //       'emits [NameVotesLoaded] with user votes when the name votes repository publishes my votes',
-    //       build: () async {
-    //         when(nameVotesRepository.all())
-    //             .thenAnswer((realInvocation) => Stream.empty());
-    //         when(nameVotesRepository.my())
-    //             .thenAnswer((realInvocation) => Stream.value(userVotes));
-    //         return NameVotesBloc(nameVotesRepository: nameVotesRepository);
-    //       },
-    //       act: (bloc) => bloc.add(LoadNameVotes()),
-    //       wait: const Duration(milliseconds: 0),
-    //       expect: [NameVotesLoaded(nameVotes: [], userVotes: userVotes)]);
+    // the blocTest doesn't yet allow to wait for all times, so to test
+    // if the internal stream subscriptions work properly and actually publish
+    // accurate events and then states, we're using fake_async library and flushing all times in the "act" callback
+    fakeAsync((async) {
+      blocTest(
+          'emits [NameVotesLoaded] with user votes when the name votes repository publishes my votes',
+          build: () async {
+        when(nameVotesRepository.all())
+            .thenAnswer((realInvocation) => Stream.empty());
+        when(nameVotesRepository.my())
+            .thenAnswer((realInvocation) => Stream.value(userVotes));
+        return NameVotesBloc(nameVotesRepository: nameVotesRepository);
+      }, act: (bloc) async {
+        bloc.add(LoadNameVotes());
+        async.flushTimers();
+      },
+          wait: const Duration(milliseconds: 0),
+          expect: [NameVotesLoaded(userVotes: userVotes)]);
+    });
+
+    fakeAsync((async) {
+      blocTest(
+          'emits [NameVotesLoaded, NameVotesLoaded] with name and user votes when the name votes repository publishes all votes and my votes',
+          build: () async {
+        when(nameVotesRepository.all())
+            .thenAnswer((realInvocation) => Stream.value(nameVotes));
+        when(nameVotesRepository.my())
+            .thenAnswer((realInvocation) => Stream.value(userVotes));
+        return NameVotesBloc(nameVotesRepository: nameVotesRepository);
+      }, act: (bloc) async {
+        bloc.add(LoadNameVotes());
+        async.flushTimers();
+      }, wait: const Duration(milliseconds: 0), expect: [
+        NameVotesLoaded(nameVotes: nameVotes),
+        NameVotesLoaded(nameVotes: nameVotes, userVotes: userVotes)
+      ]);
+    });
   });
 }
