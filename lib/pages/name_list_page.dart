a265668294094bd4c57f55a8ed453719824517ab
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2020-05-25 15:10:24 +0200

    Submitting new vote works for my user votes

--- lib/pages/name_list_page.dart
+++ lib/pages/name_list_page.dart
@@ -24,15 +24,46 @@ class NameListPage extends StatelessWidget {
   Widget _buildList(
       BuildContext context, List<NameVote> votes, UserVotes userVotes) {
     return votes.length > 0
-        ? ListView(
-            padding: const EdgeInsets.only(top: 20.0),
-            children: votes
-                .map((data) =>
-                    _buildListItem(context, data, userVotes.hasVote(data.id)))
-                .toList())
+        ? buildVotesPage(context, votes, userVotes)
         : Center(
             child: Text('No names available to vote'),
           );
+  }
+
+  Widget buildVotesPage(
+      BuildContext context, List<NameVote> votes, UserVotes userVotes) {
+    final bloc = BlocProvider.of<NameVotesBloc>(context);
+    return Column(
+      children: [
+        Expanded(
+            child: ListView(
+                padding: const EdgeInsets.only(top: 20.0),
+                children: votes
+                    .map((data) => _buildListItem(
+                        context, data, userVotes.hasVote(data.id)))
+                    .toList())),
+        Row(
+          children: [
+            Expanded(
+              child: RaisedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (context) {
+                      return NewNameDialog(
+                        bloc: bloc,
+                      );
+                    },
+                  );
+                },
+                child: Text('Submit a name'),
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 
   Widget _buildListItem(
@@ -53,6 +84,56 @@ class NameListPage extends StatelessWidget {
             BlocProvider.of<NameVotesBloc>(context).add(SubmitNameVote(record));
           },
         ),
+      ),
+    );
+  }
+}
+
+class NewNameDialog extends StatefulWidget {
+  final NameVotesBloc bloc;
+
+  const NewNameDialog({Key key, @required this.bloc}) : super(key: key);
+
+  @override
+  _NewNameDialogState createState() => _NewNameDialogState(bloc);
+}
+
+class _NewNameDialogState extends State<NewNameDialog> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final newNameTextController = TextEditingController();
+  final NameVotesBloc bloc;
+
+  _NewNameDialogState(this.bloc);
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    newNameTextController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.all(5),
+              child: TextField(
+                controller: newNameTextController,
+              )),
+          RaisedButton(
+            onPressed: () {
+              bloc.add(SubmitNameVote(NameVote(
+                  id: newNameTextController.text.toLowerCase(),
+                  name: newNameTextController.text,
+                  votes: 0)));
+              Navigator.of(context).pop();
+            },
+            child: Text('Save'),
+          )
+        ],
       ),
     );
   }
