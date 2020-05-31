cbf73bbd8330abda8f9af1aeea80993d524a83cb
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2020-05-31 18:15:16 +0200

    Refactored the dialog widget to a presentational widget Handling proposing a new name in the NameListPage smart widget

--- lib/pages/name_list_page.dart
+++ lib/pages/name_list_page.dart
@@ -32,7 +32,6 @@ class NameListPage extends StatelessWidget {
 
   Widget buildVotesPage(
       BuildContext context, List<NameVote> votes, UserVotes userVotes) {
-    final bloc = BlocProvider.of<NameVotesBloc>(context);
     return Column(
       children: [
         Expanded(
@@ -42,28 +41,37 @@ class NameListPage extends StatelessWidget {
                     .map((data) => _buildListItem(
                         context, data, userVotes.hasVote(data.id)))
                     .toList())),
-        Row(
-          children: [
-            Expanded(
-                child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: RaisedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    barrierDismissible: false,
-                    builder: (context) {
-                      return NewNameDialog(
-                        bloc: bloc,
-                      );
-                    },
+        buildProposeButton(context),
+      ],
+    );
+  }
+
+  Widget buildProposeButton(BuildContext context) {
+    final proposeNameHandler = (String name) {
+      BlocProvider.of<NameVotesBloc>(context).add(SubmitNameVote(
+          NameVote(id: name.toLowerCase(), name: name, votes: 0)));
+    };
+
+    return Row(
+      children: [
+        Expanded(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: RaisedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (context) {
+                  return NewNameDialog(
+                    onNamePropose: proposeNameHandler,
                   );
                 },
-                child: Text('Propose a new name'),
-              ),
-            ))
-          ],
-        )
+              );
+            },
+            child: Text('Propose a new name'),
+          ),
+        ))
       ],
     );
   }
@@ -92,21 +100,26 @@ class NameListPage extends StatelessWidget {
 }
 
 class NewNameDialog extends StatefulWidget {
-  final NameVotesBloc bloc;
+  final Function(String) onNamePropose;
+  final Function onCancel;
 
-  const NewNameDialog({Key key, @required this.bloc}) : super(key: key);
+  const NewNameDialog({Key key, this.onNamePropose, this.onCancel})
+      : super(key: key);
 
   @override
-  _NewNameDialogState createState() => _NewNameDialogState(bloc);
+  _NewNameDialogState createState() =>
+      _NewNameDialogState(onCancel: onCancel, onNamePropose: onNamePropose);
 }
 
 class _NewNameDialogState extends State<NewNameDialog> {
+  final Function(String) onNamePropose;
+  final Function onCancel;
+
   // Create a text controller and use it to retrieve the current value
   // of the TextField.
   final newNameTextController = TextEditingController();
-  final NameVotesBloc bloc;
 
-  _NewNameDialogState(this.bloc);
+  _NewNameDialogState({this.onNamePropose, this.onCancel});
 
   @override
   void dispose() {
@@ -125,10 +138,14 @@ class _NewNameDialogState extends State<NewNameDialog> {
       actions: [
         FlatButton(
           onPressed: () {
-            bloc.add(SubmitNameVote(NameVote(
-                id: newNameTextController.text.toLowerCase(),
-                name: newNameTextController.text,
-                votes: 0)));
+            onCancel?.call();
+            Navigator.of(context).pop();
+          },
+          child: Text('Cancel'),
+        ),
+        RaisedButton(
+          onPressed: () {
+            onNamePropose?.call(newNameTextController.text);
             Navigator.of(context).pop();
           },
           child: Text('Propose'),
